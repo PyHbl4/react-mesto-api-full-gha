a4d7e4de69abb875ffb1a77b3e020af80b9f57d2
PyHbl4 <maxim_runkov@mail.ru>
2023-08-15 21:14:43 +0500

    fix: исправление багов по ревью

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,12 +1,12 @@
 export const initialInfo = {
-    apiUrl: 'http://api.maxim-runkov.nomoreparties.co',
+    apiUrl: 'https://api.maxim-runkov.nomoreparties.co',
     // apiUrl: 'http://127.0.0.1:3000',
     pathToCards: '/cards',
     pathToMyCard: '/users/me',
     pathToMyAvatar: '/avatar'
 };
 export const authApiInfo = {
-    baseUrl: 'http://api.maxim-runkov.nomoreparties.co',
+    baseUrl: 'https://api.maxim-runkov.nomoreparties.co',
     // baseUrl: 'http://127.0.0.1:3000',
     authUrl: '/signin',
     pathToMyCard: '/users/me',
